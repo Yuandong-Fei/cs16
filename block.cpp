f69d0314f9595a577cf4e0786f61b404483de221
H2O <samuelfei2000@gmail.com>
2019-02-02 12:15:43 -0800

    StarT

--- block.cpp
+++ block.cpp
@@ -6,13 +6,12 @@ int main()
     	string question = "Enter number of rows and columns: \n";
 	int var1,var2;
 	// var1 = row; var2 = column;
-	cout<< question << endl;
+	cout<< question;
 	cin>> var1 >> var2;
 
 	for(int y = var1; y > 0; y--)
 	{
 		int x = var2;
-		cout<<"\n";
 		if(x == 0)
 		{
 			break;
@@ -23,7 +22,9 @@ int main()
 			x-=1;
 		}
 	}
-
+	cout<<"\n";
+	cout<<question;
+	cin>> var1>> var2;
 	return 0;
 
 }
